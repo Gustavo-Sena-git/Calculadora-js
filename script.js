5b1c2613f89492278b6746f934b111f3871286de
gustavo-sena-git <gustavo_sena-alves@estudante.sesisenai.org.br>
2026-03-20 09:08:43 -0300

    ADD-FIz uma calculadora mais simples onde o usuario adiciona um numero aperta na operação que deseja adiciona outro numero e aperta em igual e aparece o resultado

--- script.js
+++ script.js
@@ -22,3 +22,31 @@ function calcular(){
         document.getElementById('resultado').innerHTML = "Erro"
     }
 }
+
+let number1 = 0;
+let number2 = 0;
+let operador = '';
+
+
+function insert(op) {
+    operador = op;
+}
+
+function calcular(){
+    number1 = parseFloat(document.querySelector('.number1').value);
+    number2 = parseFloat(document.querySelector('.number2').value);
+
+    let resultado
+
+    if (operador === '+') {
+        resultado = number1 + number2;
+    } else if (operador === '-') {
+        resultado = number1 - number2;
+    } else if (operador === '*') {
+        resultado = number1 * number2;
+    } else if (operador === '/') {
+        resultado = number1 / number2;
+    }
+
+    document.getElementById('result').textContent = "Resultado:" + resultado 
+}
